8f902cc5f635e7ccdd49dfe0df3d2c085e7f5efe
Chairal Octavyanz <chairaloktovian@gmail.com>
2026-05-13 14:48:24 +0700

    Initial project setup

---
+++ CampusGo/CampusGo/CampusGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CampusGoApp.swift
+//  CampusGo
+//
+//  Created by Chairal Octavyanz on 13/05/26.
+//
+
+import SwiftUI
+
+@main
+struct CampusGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
