62806279eff31c064ee8de04faefa6c6477efb65
Chairal Octavyanz <chairaloktovian@gmail.com>
2026-05-13 15:41:36 +0700

    feat: user login screen

---
+++ CampusGo/CampusGo/Views/UserLogin/UserLoginViews.swift
@@ -0,0 +1,24 @@
+//
+//  UserLoginViews.swift
+//  CampusGo
+//
+//  Created by Chairal Octavyanz on 13/05/26.
+//
+
+import SwiftUI
+
+struct UserLoginViews: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("UserLoginViews")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    UserLoginViews()
+}
