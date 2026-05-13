af853ade4731c8896921f8a6518e3c22a26349dd
Chairal Octavyanz <chairaloktovian@gmail.com>
2026-05-13 16:00:08 +0700

    feat: add user consent ui screen

---
+++ CampusGo/CampusGo/Views/UserConsent/UserConsent.swift
@@ -0,0 +1,24 @@
+//
+//  UserConsent.swift
+//  CampusGo
+//
+//  Created by Chairal Octavyanz on 13/05/26.
+//
+
+import SwiftUI
+
+struct UserConsent: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("UserConsent")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    UserConsent()
+}
