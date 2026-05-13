6b0dfa11df6b696418b6ef4006d4adc67d79ef8a
Chairal Octavyanz <chairaloktovian@gmail.com>
2026-05-13 15:48:54 +0700

    feat: add timetable uni screen

---
+++ CampusGo/CampusGo/Views/TimeTable/TimeTableViews.swift
@@ -0,0 +1,24 @@
+//
+//  TimeTableViews.swift
+//  CampusGo
+//
+//  Created by Chairal Octavyanz on 13/05/26.
+//
+
+import SwiftUI
+
+struct TimeTableViews: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("TimeTableViews")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    TimeTableViews()
+}
